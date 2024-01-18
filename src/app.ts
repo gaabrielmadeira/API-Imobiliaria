14080f4bf967358f7f76fe2a90b86bee13fe93fd
Gabriel Madeira <gabscastello95@gmailcom>
2024-01-18 14:57:06 -0300

    finish

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import "express-async-errors";
 import express from "express";
 import { CategoriesRouter, RealEstateRouter, SchedulesRouter, UserRouter, sessionRouter } from "./routes";
 import { handleErrors } from "./middlewares";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +14,7 @@ app.use("/login", sessionRouter);
 app.use("/categories", CategoriesRouter);
 app.use("/realEstate", RealEstateRouter);
 app.use("/schedules", SchedulesRouter);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use(handleErrors);
 
